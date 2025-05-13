e32338503c7db92cab4b6952812b8d3df172718e
real-chicken-master <aomkennedy@gmail.com>
2025-05-13 13:10:02 +1200

    adding files

---
+++ tetris-java/gui.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.image.BufferedImage;
+/**
+ * Write a description of class gui here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class gui extends JFrame implements ActionListener {
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+    Canvas screen;
+    BufferedImage offScreenImage;
+    int windowHeight = 800;
+    int windowLength = 1310;
+    int imageHeight = 40;
+    int imageWidth = 40;
+    ImageIcon BlackSquare =  new ImageIcon(new ImageIcon("images/BlackSquare.PNG").getImage().getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT));
+    ImageIcon BlueSquare =  new ImageIcon(new ImageIcon("images/BlueSquare.PNG").getImage().getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT));   
+    /**
+     * Constructor for objects of class gui
+     */
+    public gui()
+    {
+        setTitle("tetris");
+
+        this.getContentPane().setPreferredSize(new Dimension(windowLength,windowHeight));
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.toFront();
+
+        this.setVisible(true);
+
+        menuBar=new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        menu = new JMenu(" Menu ");
+        menuBar.add(menu);
+
+        menuItem=new JMenuItem("restart");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        menuItem=new JMenuItem("update");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(windowLength,windowHeight));
+        screen = new Canvas();
+        panel.add(screen);
+        this.pack();
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String cmd=e.getActionCommand();
+        repaint();
+    }
+
+}    
---
+++ tetris-java/tetris.java
@@ -0,0 +1,94 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+
+/**
+ * Write a description of class tetris here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class tetris extends gui
+{
+    // instance variables - replace the example below with your own
+    int tetrisWidth = 10;
+    int tetrisHeight = 20;
+    private ImageIcon ImageArray[][] = new ImageIcon [tetrisWidth][tetrisHeight];
+    String PlacedBlockArray[][] = new String  [tetrisWidth][tetrisHeight];
+    private int boardWidth = windowLength/3;
+    private int boardX = (windowLength-boardWidth)/2;
+    String FallingBlock = "empty";
+    double FallingBlockX;
+    double FallingBlockY;
+    boolean BlockFalling = false;
+    /**
+     * Constructor for objects of class tetris
+     */
+    public tetris()
+    {   
+        setup();
+        while(true){
+            repaint();
+        }
+    }
+
+    void clearBoard(){
+        for(int x=0; x<tetrisWidth; x++){
+            for(int y=0; y<tetrisHeight; y++){
+                ImageArray[x][y] = BlackSquare;
+                PlacedBlockArray[x][y] = "empty";
+            }
+        }
+    }
+
+    private void setup(){
+        clearBoard();
+    }
+
+    public void paint (Graphics g){
+        clearBoard();
+        controlBlocks();
+        offScreenImage = new BufferedImage(windowLength+10 ,windowHeight+100,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = (Graphics2D) offScreenImage.getGraphics();
+        //white background
+        g2.setColor(new Color(255,255,255));
+        g2.fillRect(0,0,windowLength+10,windowHeight+100);
+        //tetris background
+        g2.setColor(new Color(0,0,0));
+        for(int x=0; x<tetrisWidth; x++){
+            for(int y=0; y<tetrisHeight; y++){
+                ImageArray[x][y].paintIcon(this,g2,100+x*imageWidth,y*imageHeight+imageHeight);
+            }
+        }
+        g.drawImage(offScreenImage,0,0,null);
+    }
+
+    void controlBlocks(){
+        if(BlockFalling){
+            moveBlocks();
+        }else{
+            addBlocks();
+        }
+    }
+
+    void addBlocks(){
+        FallingBlockY = 0;
+        FallingBlockX = 4;
+        FallingBlock = "Square";
+        BlockFalling = true;
+    }
+
+    void moveBlocks(){
+        FallingBlockY += 0.01;
+        drawSquare((int)FallingBlockX,(int)FallingBlockY);
+    }
+
+    void drawSquare(int x, int y){
+        ImageArray[x][y] = BlueSquare;
+        ImageArray[x+1][y] = BlueSquare;
+        ImageArray[x+1][y+1] = BlueSquare;
+        ImageArray[x][y+1] = BlueSquare;
+    }
+}
